7bf4074259a96ce21a8e764ea209e8777f241951
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-11-27 16:12:19 +0700

    Increase sqrt precision

--- term-project.srcs/sources_1/new/sqrt.v
+++ term-project.srcs/sources_1/new/sqrt.v
@@ -29,15 +29,15 @@ module sqrt(
     input [47:0] d
     );
     
-    assign done = count == 8'd24;
+    assign done = count == 8'd48;
     reg busy = 0;
     
     reg [7:0] count;
-    reg [23:0] q;
-    reg [24:0] r;
-    reg [47:0] tmp_d;
+    reg [47:0] q;
+    reg [48:0] r;
+    reg [95:0] tmp_d;
     
-    assign s = { 12'd0, q, 12'd0 };
+    assign s = q;
     
     always @(posedge clk)
     begin
@@ -46,24 +46,24 @@ module sqrt(
             count <= 0;
             q <= 0;
             r <= 0;
-            tmp_d <= d; 
+            tmp_d <= { 24'd0, d, 24'd0 }; 
         end
         if (busy & ~done) begin
-            if (~r[24]) begin
-                r = { r[22:0], tmp_d[47:46] };
-                r = r - { q[21:0], 2'd1 };
+            if (~r[48]) begin
+                r = { r[46:0], tmp_d[95:94] };
+                r = r - { q[45:0], 2'd1 };
             end
             else begin
-                r = { r[22:0], tmp_d[47:46] };
-                r = r + { q[21:0], 2'd3 };
+                r = { r[46:0], tmp_d[95:94] };
+                r = r + { q[45:0], 2'd3 };
             end
-            if (~r[24]) begin
-                q = { q[22:0], 1'd1 };
+            if (~r[48]) begin
+                q = { q[46:0], 1'd1 };
             end
             else begin
-                q = { q[22:0], 1'd0 };
+                q = { q[46:0], 1'd0 };
             end
-            tmp_d = { tmp_d[45:0], 2'd0 };
+            tmp_d = { tmp_d[93:0], 2'd0 };
             count = count + 1;
         end
         if (busy & done) busy <= 0;
